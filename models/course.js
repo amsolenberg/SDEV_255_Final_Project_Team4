c82a55b9204c765ca1bfc1c3b9dbb4fdd48d984c
Aaron Solenberg <amsolenberg@gmail.com>
2024-12-05 12:37:49 -0500

    Updated course schema to include `createdBy`

--- models/course.js
+++ models/course.js
@@ -1,29 +1,37 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const courseSchema = new Schema({
+const courseSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true
     },
     code: {
-        type: String,
-        required: true,
-        uppercase: true
+      type: String,
+      required: true,
+      uppercase: true
     },
     description: {
-        type: String,
-        required: true,
+      type: String,
+      required: true
     },
     credits: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true
     },
     faculty: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
     }
-}, {timestamps: true});
+  },
+  { timestamps: true }
+);
 
-const Course = mongoose.models.Course ||  mongoose.model('Course', courseSchema);
+const Course = mongoose.models.Course || mongoose.model('Course', courseSchema);
 module.exports = Course;
